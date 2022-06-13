cc25e0a748781ef3bc6dfaab059921be5d6eccbb
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-13 16:38:51 -0400

    create new flash cards

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from tkinter import *
-from numpy import pad
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Ariel"
+
+
+# ---------------------------- CREATE NEW FLASH CARDS ------------------------------- # 
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -12,19 +24,20 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas= Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=(FONT_NAME, 40, "italic"), fill="black")
-canvas.create_text(400, 263, text="word", font=(FONT_NAME, 40, "bold"), fill="black")
+card_title = canvas.create_text(400, 150, text="", font=(FONT_NAME, 40, "italic"), fill="black")
+card_word = canvas.create_text(400, 263, text="", font=(FONT_NAME, 40, "bold"), fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 
-
 #Buttons
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image,highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+right_button = Button(image=right_image,highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
 right_button.grid(row=1, column=0)
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR)
+wrong_button = Button(image=wrong_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command= next_card)
 wrong_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
